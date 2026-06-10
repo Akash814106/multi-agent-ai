afb264f03d2c3ee4519955a6496707f533dd8483
Akash814106 <akashshety1998@gmail.com>
2026-06-10 22:54:09 +0530

    Refactor workflow into reusable orchestrator

---
+++ backend/test_workflow_v2.py
@@ -0,0 +1,8 @@
+from workflows.multi_agent_workflow import run_workflow
+
+result = run_workflow(
+    "What should i learn next?"
+)
+
+print("\n---Result---\n")
+print(result)
---
+++ backend/workflows/multi_agent_workflow.py
@@ -0,0 +1,59 @@
+from memory.chroma_memory import retrieve_memory
+
+from agents.planner_agent import planner_agent
+from agents.research_agent import research_agent
+from agents.critic_agent import critic_agent
+
+from utils.task_parser import extract_tasks
+
+def run_workflow(user_query):
+
+    all_results = []
+
+    #Retrieve memory
+    memories = retrieve_memory(user_query)
+
+    if memories and memories[0]:
+        memory_context = "\n".join(memories[0])
+    else:
+        memory_context=""
+
+    #Combine memory and user query
+    enhanced_query = f"""
+
+    Memory : 
+    {memory_context}
+    Question :
+    {user_query}
+    """
+
+    #Planner agent
+    plan = planner_agent(enhanced_query)
+
+    #Extract tasks from plan
+    tasks = extract_tasks(plan)
+
+    #Send task to research agent and research output to critic agent
+
+    for task in tasks:
+
+        research_result = research_agent(task)
+        critic_result = critic_agent(research_result)
+
+        all_results.append(
+            {
+                "task":task,
+                "research":research_result,
+                "critic":critic_result
+            }
+        )
+
+    return {
+        "memory":memory_context,
+        "plan":plan,
+        "tasks":tasks,
+        "results":all_results
+    }      
+
+
+    

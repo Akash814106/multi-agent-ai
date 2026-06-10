d4664fdb0a024328201313c337666e84a66ba4e7
Akash814106 <akashshety1998@gmail.com>
2026-06-10 21:02:48 +0530

    Build memory-aware multi-agent workflow

---
+++ backend/test_complete_workflow.py
@@ -0,0 +1,56 @@
+from memory.chroma_memory import retrieve_memory
+
+from agents.planner_agent import planner_agent
+from agents.research_agent import research_agent
+from agents.critic_agent import critic_agent
+
+from utils.task_parser import extract_tasks
+
+
+user_query = "What should i learn next?"
+
+memories = retrieve_memory(user_query)
+memory_context = "\n".join(memories[0])
+print("\n Memory context : \n")
+print(memory_context)
+
+print("\n"+"="*80)
+
+enhanced_query = f"""
+Memory : {memory_context}
+
+Question : {user_query}
+"""
+print("\nEnhanced Query : \n")
+print(enhanced_query)
+
+
+print("\n"+"="*80)
+
+
+plan = planner_agent(enhanced_query)
+print("\n---Plan---\n")
+print(plan)
+
+
+print("\n"+"="*80)
+
+
+tasks = extract_tasks(plan)
+print("\nExtracted taasks\n")
+print(tasks)
+
+print("\n"+"="*80)
+
+
+for task in tasks:
+
+    research_result = research_agent(task)
+    print("\n---Research result---\n")
+    print(research_result)
+
+    critic_result = critic_agent(research_result)
+    print("\n---Critic result---\n")
+    print(critic_result)
+
+    print("\n"+"="*80)
